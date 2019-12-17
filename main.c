3a950f278ecf5e168d9de7ffd3a70e972bef76b9
kerkmyman <56095293+kerkmyman@users.noreply.github.com>
2019-12-17 17:00:24 -0500

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  test1
+//
+//  Created by Savitha Munirajaiah on 12/17/19.
+//  Copyright © 2019 Savitha Munirajaiah. All rights reserved.
+//
+
+#include "main.h"
+int main(void)
+{
+    printf("Hello World!");
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,14 @@
+//
+//  main.h
+//  test1
+//
+//  Created by Savitha Munirajaiah on 12/17/19.
+//  Copyright © 2019 Savitha Munirajaiah. All rights reserved.
+//
+
+#ifndef main_h
+#define main_h
+
+#include <stdio.h>
+
+#endif /* main_h */
